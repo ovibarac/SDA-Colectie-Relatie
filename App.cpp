fe8a5652bff15ab99917c57b210115f94256fd03
Ovidiu Barac <ovy.barak@gmail.com>
2022-04-05 11:56:24 +0300

    Initial commit

---
+++ App.cpp
@@ -0,0 +1,16 @@
+/*
+ * 6.TAD Colecțiecu elemente de tip comparabil (TElement=TComparabil)–reprezentare folosind o LDI cu elementele ordonate în raport cu relația de ordine =.
+ */
+
+#include <iostream>
+
+//#include "TestExtins.h"
+//#include "TestScurt.h"
+
+using namespace std;
+
+
+int main() {
+	//testAllExtins();
+	//testAll();
+}
